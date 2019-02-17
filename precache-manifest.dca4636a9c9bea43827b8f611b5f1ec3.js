f927ad4af2c499886284d05766550e5f28fd8ea7
Vlad <excelent91@mail.ru>
2019-02-17 21:01:45 +0200

    Updates

---
+++ precache-manifest.dca4636a9c9bea43827b8f611b5f1ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c2e27197b3338a18e7b",
+    "url": "/ContactManager/static/js/runtime~main.8c2e2719.js"
+  },
+  {
+    "revision": "224c25e480cc7746d287",
+    "url": "/ContactManager/static/js/main.224c25e4.chunk.js"
+  },
+  {
+    "revision": "8d9e6d09cb02b7049be6",
+    "url": "/ContactManager/static/js/2.8d9e6d09.chunk.js"
+  },
+  {
+    "revision": "224c25e480cc7746d287",
+    "url": "/ContactManager/static/css/main.cd57d878.chunk.css"
+  },
+  {
+    "revision": "8d9e6d09cb02b7049be6",
+    "url": "/ContactManager/static/css/2.d391443d.chunk.css"
+  },
+  {
+    "revision": "0e540646d35f1a5c3f652fc4f5b1e624",
+    "url": "/ContactManager/index.html"
+  }
+];
